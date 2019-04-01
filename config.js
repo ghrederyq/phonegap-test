fd1678581580ac52cdd6d483d6bbf29520d30ec3
ghrederyq <49131146+ghrederyq@users.noreply.github.com>
2019-04-01 23:57:22 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,23 @@
+define( function ( require ) {
+
+	"use strict";
+
+	return {
+		app_slug : 'freddie-news-app',
+		wp_ws_url : 'http://freddie.ultimatefreehost.in/newssite/wp-appkit-api/freddie-news-app',
+		wp_url : 'http://freddie.ultimatefreehost.in/newssite',
+		theme : 'q-android',
+		version : '1.0',
+		app_type : 'phonegap-build',
+		app_title : 'Freddie News App',
+		app_platform : 'android',
+		app_path: '',
+		gmt_offset : 0,
+		debug_mode : 'off',
+		auth_key : 'eSq{yUdg>H,~J44Leq:uc|/3Dky8RTkksij&jH=|Me.3/v{7ZH5F/nWXYKzZ[hw#',
+		options : {"refresh_interval":0},
+		theme_settings : [],
+		addons : []
+	};
+
+});
